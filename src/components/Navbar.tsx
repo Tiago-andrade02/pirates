0b48577f134a905aa0f4911e382b2a79fb3b595c
Tiago Andrade <tiagee88@hotmail.com>
2026-08-22 20:36:58 -0300

    fix: smaller navbar logo on desktop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,7 +50,7 @@ export function Navbar() {
           <img
             src="/nav-logo.png"
             alt="PIRATES"
-            className="h-8 w-auto object-contain"
+            className="h-8 w-auto object-contain lg:h-6"
           />
         </Link>
 
