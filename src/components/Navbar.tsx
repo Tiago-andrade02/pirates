644d42f3320b01db0cb8798c143e2f9ad25be109
Tiago Andrade <tiagee88@hotmail.com>
2026-08-22 21:07:50 -0300

    fix: navbar logo h-8 on all screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,7 +50,7 @@ export function Navbar() {
           <img
             src="/nav-logo.png"
             alt="PIRATES"
-            className="h-8 w-auto object-contain lg:h-7"
+            className="h-8 w-auto object-contain"
           />
         </Link>
 
